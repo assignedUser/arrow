76b5fb886cecc549acac6277d409a948abec1e60
Alvaro Maldonado Mateos <alvaro@voltrondata.com>
2022-08-12 11:03:08 +0200

    added encoding process for the bot

--- dev/bot/app.py
+++ dev/bot/app.py
@@ -1,12 +1,30 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 import os
 import json
+import base64
 from github import GithubIntegration
 
 
 def handler(event, context):
 
     app_id = os.environ["BOT_ID"]
-    app_key = os.environ["BOT_CERT"]
+    app_key_encoded = os.environ["BOT_CERT"]
+    app_key = base64.b64decode(app_key_encoded)
 
     git_integration = GithubIntegration(
         app_id,
